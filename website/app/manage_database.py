70c323281bb7c8e09f11f623af52bcec8088fc47
Shewi <anh.kiem.pho1@gmail.com>
2024-03-09 17:38:24 +0100

    Add an interactive card form page

---
+++ website/app/manage_database.py
@@ -0,0 +1,53 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host='localhost',
+  user="root",
+  password = '!nxbTjiPw7@Pb8', 
+  database = "cards"
+)
+
+print(mydb)
+cursor = mydb.cursor()
+cursor.execute("SELECT * FROM cards")
+
+result = cursor.fetchall()
+
+for i in result:
+    print(i,type(i))
+
+class Card:
+    def __init__(self,id) -> None:
+        self.id = id
+        #...
+
+def create_card(table="cards",id=1,deck="Japanese",front="front",front_sub="front_sub",back="back",back_sub="back_sub",back_sub2="back_sub2",tag="tag",user_id=1):
+    print(f'INSERT INTO {table} V ALUES ({id},"{deck}","{front}","{front_sub}","{back}","{back_sub}","{back_sub2}","{tag}",{user_id});')
+    cursor.execute(f'INSERT INTO {table} VALUES ({id},"{deck}","{front}","{front_sub}","{back}","{back_sub}","{back_sub2}","{tag}",{user_id});')
+    cursor.execute(f"CREATE TABLE card_{id} (rating ENUM('Again','Hard','Good','Easy'),rated_date TIMESTAMP(0), due_date TIMESTAMP(0));") #timestamp(0)=pas de décimales dans les secondes
+
+def delete_card(id):
+    cursor.execute(f"DELETE FROM cards WHERE id = {id};")
+    cursor.execute(f"DROP TABLE card_{id};")
+
+def get_all_ids(table="cards")  -> list:
+    ids = []
+    cursor.execute("SELECT * FROM cards")
+    result = cursor.fetchall()
+    for row in result:
+        ids.append(row[0])
+    return ids
+
+def get_free_id(table="cards") -> int:
+    ids = get_all_ids()
+    if len(ids) == 0:
+        return 1
+    ids.sort()
+    for id in ids:
+        if id+1 not in ids:
+            return id+1
+
+def delete_all_cards(table="cards"):
+    ids = get_all_ids()
+    for id in ids:
+        delete_card(id=id)
--- website/flask_app.py
+++ website/flask_app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from app.config import Config
 from app.card_form import Cardform
+from app.manage_database import *
+
 app = Flask(__name__)
 app.config.from_object(Config)
+
 @app.route('/')
 
 def index():
@@ -13,11 +16,16 @@ def index():
 def home(name):
     return render_template('home.html', name=name)
 
-@app.route('/formtest')
+@app.route('/formtest', methods=['GET', 'POST'])
 
 def cardform():
     form = Cardform()
-    return render_template('form.html',title='Créer une carte !',form=form)
+    if form.validate_on_submit():
+        print(form.deck.data,form.front.data,form.front_sub.data,form.back.data,form.back_sub2.data,form.tag.data)
+        create_card(id=get_free_id(),deck=form.deck.data,front=form.front.data,front_sub=form.front_sub.data,back=form.back.data,back_sub=form.back_sub.data,back_sub2=form.back_sub2.data,tag=form.tag.data)
+        flash(f'La carte {form.front.data} a bien été enregistré dans le deck {form.deck.data} !')
+        return redirect('/formtest')
+    return render_template('form.html',title='Créer une carte',form=form)
 
 
 
